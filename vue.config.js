9852b1eaa28d3a8f924238930fbe7c57f3cacf94
x1frm <alex-shvets@yandex.ru>
2020-10-04 12:22:32 +0300

    feat: Dev server config

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,10 @@ module.exports = {
                 prependData: `@import "~@/scss/variables.scss";`
             }
         }
+    },
+    devServer: {
+        port: 9000,
+        host: '0.0.0.0',
+        disableHostCheck: true
     }
 }
